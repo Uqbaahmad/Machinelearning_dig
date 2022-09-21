35b1f709b343e96d1122ea671e4486f7b3420028
Uqba Ahmad <104266403+Uqbaahmad@users.noreply.github.com>
2022-09-21 22:42:04 +0530

    commit

--- churn_prediction_app/app.py
+++ churn_prediction_app/app.py
@@ -15,19 +15,7 @@ st.set_page_config(
     layout="wide",
     page_icon=""
 )    
-'''
-RowNumber                  1
-CreditScore              619
-Geography             France
-Gender                Female
-Age                       42
-Tenure                     2
-Balance                  0.0
-NumOfProducts              1
-HasCrCard                  1
-IsActiveMember             1
-EstimatedSalary    101348.88
-'''
+
 
 
 with st.form('form1', clear_on_submit=True):
@@ -36,11 +24,11 @@ with st.form('form1', clear_on_submit=True):
     gender = st.radio('Gender',['Feamle', 'Male'])
     age = st.number_input("Age", min_value=18, max_value=100, value=33)
     tenure = st.number_input("Tensure", min_value=0, max_value=100, value=2)
-    balance = st.number_input("Balance", min_value=0.0, max_value=9999999, value=100.0, step=.5)
+    balance = st.number_input("Balance", min_value=0.0, max_value=9999999.0, value=100.0, step=.5)
     num_products = st.radio("Num of products", [1,2,3,4])
     has_card =st.checkbox("Has Credit card", value=True)
     is_active = st.checkbox("Is active member", value=True)
-    est_salary = st.slider("Estimted Salary", min_value=1000, max_value=999999, value = 2000, step=.5)
+    est_salary = st.slider("Estimted Salary", min_value=1000.0, max_value=999999.0, value = 2000.0, step=.5)
 
     btn = st.form_submit_button("Predict Customer Churn Status")
 
